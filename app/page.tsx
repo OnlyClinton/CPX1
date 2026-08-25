56ee072f9c822078e66a62282e50308fd7d8c9f2
OnlyClinton <120651995+OnlyClinton@users.noreply.github.com>
2026-08-24 23:36:37 -0400

    Apply final WDCC CTA hierarchy

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,2 @@
-import R31R25Home from "./R31R25Home";
-export default function Home(){return <R31R25Home/>}
+import R31R25Home from "./R31R25Home";import CtaRewire from"./CtaRewire";
+export default function Home(){return <><R31R25Home/><CtaRewire/></>}
