f2b2931b5a9230a3222f9bb66e104dbe66cf5f90
OnlyClinton <120651995+OnlyClinton@users.noreply.github.com>
2026-08-25 23:46:50 -0400

    Use approved WDCC reference clone on storefront home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
-import Exact2vfDHome from "./Exact2vfDHome";
+import ReferenceCloneHome from "./ReferenceCloneHome";
 
-export default function Home(){return <Exact2vfDHome/>}
+export default function Home(){return <ReferenceCloneHome/>}
